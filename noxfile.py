19a0914be3aa40d78fd268c40e60c91044c52edb
Kirill Sulim <kirillsulim@gmail.com>
2020-07-22 00:26:13 +0500

    Add travis config

---
+++ noxfile.py
@@ -0,0 +1,43 @@
+import nox
+
+
+LINE_LENGTH = 120
+
+STYLE_TARGETS = [
+    "panamap",
+    "tests",
+    "noxfile.py",
+    "setup.py",
+]
+
+FLAKE8_IGNORE = [
+    "E203",
+    "E231",
+    "W503",
+]
+
+
+@nox.session
+def unit_tests(session):
+    session.install(".")
+    session.install("pytest")
+    session.run("pytest", "tests")
+
+
+@nox.session
+def style(session):
+    session.install("flake8", "black", "isort")
+
+    session.run("black", "--version")
+    session.run("black", "--check", "--target-version", "py38", "--line-length", f"{LINE_LENGTH}", *STYLE_TARGETS)
+
+    session.run("flake8", "--version")
+    session.run(
+        "flake8",
+        "--max-line-length",
+        f"{LINE_LENGTH}",
+        "--extend-ignore",
+        ",".join(FLAKE8_IGNORE),
+        "--show-source",
+        *STYLE_TARGETS,
+    )
